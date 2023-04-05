b25d335034f5b2a9a5aedc5de095eb31de098d89
ZhangYuan <zhangyuan@xiaozhistar.com>
2023-04-05 15:31:32 +0800

    save

--- lua/plugin-config/snippets.lua
+++ lua/plugin-config/snippets.lua
@@ -30,14 +30,60 @@ local postfix = require "luasnip.extras.postfix".postfix
 --     key = "all",
 -- })
 
-
 function capitalizeFirstWord(str)
   local firstChar = string.sub(str, 1, 1)
   local rest = string.sub(str, 2)
   return string.upper(firstChar) .. rest
 end
 
+function getFilename(path)
+  local name = string.match(path, ".*/([^/]+)")
+  if name == nil then
+    return ""
+  else
+    name = string.match(name, "^(.+)%..+$") or name
+    return name
+  end
+end
+
 ls.add_snippets("javascript", {
+    -- recoil: atom
+  s('atom', fmta([[
+  export const <> = atom({
+    key: `${BASE_FIX}<>`,
+    default: <>
+  )};
+  ]], { i(1), rep(1), i(2, 'null') })),
+
+    -- styled-components
+  s('ctom', fmt([[
+  const {} = styled.div`
+    {}
+  `;
+  ]], { i(1), i(2), })),
+
+    -- display: grid
+  s('dg', fmt([[
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  grid-gap: 20px;
+  grid-auto-flow: column;
+  justify-content: stretch;
+  justify-items: stretch;
+  align-content: stretch;
+  align-items: center;
+  {}
+  ]], { i(1) })),
+
+  -- import
+  s('imp', fmt([[import {2} from "{1}"]], { 
+    i(1),
+    f(function(args)
+      return getFilename(args[1][1] or "")
+    end
+    , {1}),
+  })),
   s('im_', t({"import _ from 'lodash';"})),
   -- useMemo
   s("uM", fmt([[
