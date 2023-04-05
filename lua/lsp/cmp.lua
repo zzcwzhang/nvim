a04390da220c0c5bc682226c3fd4096cf711b9e9
ZhangYuan <zhangyuan@xiaozhistar.com>
2023-04-05 14:01:24 +0800

    配置snapshot的mapping支持<tab> 和<s-tab>

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -1,4 +1,30 @@
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+local defaultMapping = require("keybindings").cmp(cmp)
+defaultMapping["<Tab>"] = cmp.mapping(function(fallback)
+  if cmp.visible() then
+    cmp.select_next_item()
+    -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+    -- they way you will only jump inside the snippet region
+  elseif luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump()
+  elseif has_words_before() then
+    cmp.complete()
+  else
+    fallback()
+  end
+end, { "i", "s" })
+
+defaultMapping["<S-Tab>"] = cmp.mapping(function(fallback)
+  if cmp.visible() then
+    cmp.select_prev_item()
+  elseif luasnip.jumpable(-1) then
+    luasnip.jump(-1)
+  else
+    fallback()
+  end
+end, { "i", "s" })
 
 cmp.setup({
   -- 指定 snippet 引擎
@@ -15,7 +41,8 @@ cmp.setup({
   }, { { name = "buffer" }, { name = "path" } }),
 
   -- 快捷键设置
-  mapping = require("keybindings").cmp(cmp),
+  -- mapping = require("keybindings").cmp(cmp),
+  mapping = defaultMapping,
 })
 
 -- / 查找模式使用 buffer 源
@@ -32,6 +59,6 @@ cmp.setup.cmdline(":", {
   sources = cmp.config.sources({
     { name = "path" },
   }, {
-    { name = "cmdline" },
-  }),
+      { name = "cmdline" },
+    }),
 })
