666af1c29112834a0b64efc35ae2e287b81db8bd
ZhangYuan <zhangyuan@xiaozhistar.com>
2023-04-11 13:00:43 +0800

    add: 增加pwd的代码片段

--- lua/plugin-config/snippets.lua
+++ lua/plugin-config/snippets.lua
@@ -19,52 +19,70 @@ local fmta = require("luasnip.extras.fmt").fmta
 local types = require("luasnip.util.types")
 local conds = require("luasnip.extras.conditions")
 local conds_expand = require("luasnip.extras.conditions.expand")
-local postfix = require "luasnip.extras.postfix".postfix
+local postfix = require("luasnip.extras.postfix").postfix
 
--- ls.add_snippets("all", {
---   s("ternary", {
---     -- equivalent to "${1:cond} ? ${2:then} : ${3:else}"
---     i(1, "cond"), t(" ? "), i(2, "then"), t(" : "), i(3, "else")
---   })
--- }, {
---     key = "all",
--- })
+ls.add_snippets("all", {
+	s(
+		"pwd",
+		f(function()
+			return vim.fn.getreg("%")
+		end)
+	),
+}, {
+	key = "all",
+})
 
 function capitalizeFirstWord(str)
-  local firstChar = string.sub(str, 1, 1)
-  local rest = string.sub(str, 2)
-  return string.upper(firstChar) .. rest
+	local firstChar = string.sub(str, 1, 1)
+	local rest = string.sub(str, 2)
+	return string.upper(firstChar) .. rest
 end
 
 function getFilename(path)
-  local name = string.match(path, ".*/([^/]+)")
-  if name == nil then
-    return ""
-  else
-    name = string.match(name, "^(.+)%..+$") or name
-    return name
-  end
+	local name = string.match(path, ".*/([^/]+)")
+	if name == nil then
+		return ""
+	else
+		name = string.match(name, "^(.+)%..+$") or name
+		return name
+	end
 end
 
 ls.add_snippets("javascript", {
-  s('llg', fmt([[.tap((_v) => console.log("{}", _v))]], { i(1) })),
-    -- recoil: atom
-  s('atom', fmta([[
+	s("llg", fmt([[.tap((_v) => console.log("{}", _v))]], { i(1) })),
+
+	-- recoil: atom
+	s(
+		"atom",
+		fmta(
+			[[
   export const <> = atom({
     key: `${BASE_FIX}<>`,
     default: <>
   });
-  ]], { i(1), rep(1), i(2, 'null') })),
+  ]],
+			{ i(1), rep(1), i(2, "null") }
+		)
+	),
 
-    -- styled-components
-  s('ctom', fmt([[
+	-- styled-components
+	s(
+		"ctom",
+		fmt(
+			[[
   const {} = styled.div`
     {}
   `;
-  ]], { i(1), i(2), })),
+  ]],
+			{ i(1), i(2) }
+		)
+	),
 
-    -- display: grid
-  s('dg', fmt([[
+	-- display: grid
+	s(
+		"dg",
+		fmt(
+			[[
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: 1fr;
@@ -75,39 +93,63 @@ ls.add_snippets("javascript", {
   align-content: stretch;
   align-items: center;
   {}
-  ]], { i(1) })),
+  ]],
+			{ i(1) }
+		)
+	),
 
-  -- import
-  s('imp', fmt([[import {2} from '{1}';]], { 
-    i(1),
-    f(function(args)
-      return getFilename(args[1][1] or "")
-    end
-    , {1}),
-  })),
-  s('im_', t({"import _ from 'lodash';"})),
-  -- useMemo
-  s("uM", fmt([[
+	-- import
+	s(
+		"imp",
+		fmt([[import {2} from '{1}';]], {
+			i(1),
+			f(function(args)
+				return getFilename(args[1][1] or "")
+			end, { 1 }),
+		})
+	),
+	s("im_", t({ "import _ from 'lodash';" })),
+	-- useMemo
+	s(
+		"uM",
+		fmt(
+			[[
   const <> = useMemo({
     <>
   }, []);
-  ]], {
-      i(1), i(2),
-    }, {
-      delimiters = "<>"
-    })),
-  -- useCallback
-  s("uC", fmt([[
+  ]],
+			{
+				i(1),
+				i(2),
+			},
+			{
+				delimiters = "<>",
+			}
+		)
+	),
+	-- useCallback
+	s(
+		"uC",
+		fmt(
+			[[
   const <> = useCallback({
     <>
   }, []);
-  ]], {
-      i(1),i(2),
-    }, {
-      delimiters = "<>"
-    })),
-  -- fast create page
-  s("shook", fmt([[
+  ]],
+			{
+				i(1),
+				i(2),
+			},
+			{
+				delimiters = "<>",
+			}
+		)
+	),
+	-- fast create page
+	s(
+		"shook",
+		fmt(
+			[[
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
@@ -122,43 +164,53 @@ export default function []() {
 const Wrapper = styled.div`
   font-size: 20;
 `;
-  ]], {
-      i(1),
-      rep(1)
-    }, {
-      delimiters = "[]"
-    })),
-  -- useState
-  postfix("?US", {
-    f(function(_, parent)
-      local str = parent.snippet.env.POSTFIX_MATCH
-      return "const [" .. str ..", " .. "set" .. capitalizeFirstWord(str) .. "] = useState(" .. str .. ");"
-    end, {}),
-  }),
-  -- useRecoilState
-  postfix("?URS", {
-    f(function(_, parent)
-      local str = parent.snippet.env.POSTFIX_MATCH
-      return "const [" .. str ..", " .. "set" .. capitalizeFirstWord(str) .. "] = useRecoilState(" .. str .. "$);"
-    end, {}),
-  }),
-  -- useRecoilValue
-  postfix("?URV", {
-    f(function(_, parent)
-      local str = parent.snippet.env.POSTFIX_MATCH
-      return "const " .. str .. " = useRecoilValue(" .. str .. "$);"
-    end, {}),
-  }),
-  -- console.log
-  postfix("?L", {
-    f(function(_, parent)
-      local str = parent.snippet.env.POSTFIX_MATCH
-      return "console.log('" .. str .. ":', ".. str ..");"
-    end, {}),
-  }),
-
+  ]],
+			{
+				i(1),
+				rep(1),
+			},
+			{
+				delimiters = "[]",
+			}
+		)
+	),
+	-- useState
+	postfix("?US", {
+		f(function(_, parent)
+			local str = parent.snippet.env.POSTFIX_MATCH
+			return "const [" .. str .. ", " .. "set" .. capitalizeFirstWord(str) .. "] = useState(" .. str .. ");"
+		end, {}),
+	}),
+	-- useRecoilState
+	postfix("?URS", {
+		f(function(_, parent)
+			local str = parent.snippet.env.POSTFIX_MATCH
+			return "const ["
+				.. str
+				.. ", "
+				.. "set"
+				.. capitalizeFirstWord(str)
+				.. "] = useRecoilState("
+				.. str
+				.. "$);"
+		end, {}),
+	}),
+	-- useRecoilValue
+	postfix("?URV", {
+		f(function(_, parent)
+			local str = parent.snippet.env.POSTFIX_MATCH
+			return "const " .. str .. " = useRecoilValue(" .. str .. "$);"
+		end, {}),
+	}),
+	-- console.log
+	postfix("?L", {
+		f(function(_, parent)
+			local str = parent.snippet.env.POSTFIX_MATCH
+			return "console.log('" .. str .. ":', " .. str .. ");"
+		end, {}),
+	}),
 }, {
-    key = "javascript",
+	key = "javascript",
 })
 
 require("luasnip.loaders.from_lua").lazy_load({ include = { "all", "javascript" } })
