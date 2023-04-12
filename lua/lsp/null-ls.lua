d25cdb02e87e43d6f2bba7c450c27cf8091cf30f
ZhangYuan <zhangyuan@xiaozhistar.com>
2023-04-12 10:47:07 +0800

    修复自动保存

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -5,21 +5,6 @@ if not status then
 end
 
 local formatting = null_ls.builtins.formatting
-null_ls.setup({
-	debug = false,
-	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.code_actions.shellcheck,
-	},
-	-- 保存自动格式化
-	on_attach = function(client)
-		if client.server_capabilities.documentFormattingProvider then
-			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format({ async = true })")
-		end
-	end,
-})
 
 local lsp_formatting = function(bufnr)
 	vim.lsp.buf.format({
@@ -34,18 +19,40 @@ end
 -- if you want to set up formatting on save, you can use this as a callback
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-local opt = { noremap = true, silent = true }
-
+-- local opt = { noremap = true, silent = true }
 -- add to your shared on_attach callback
-local on_attach = function(client, bufnr)
-	if client.supports_method("textDocument/formatting") then
-		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = augroup,
-			buffer = bufnr,
-			callback = function()
-				lsp_formatting(bufnr)
-			end,
-		})
-	end
-end
+-- local on_attach = function(client, bufnr)
+-- 	if client.supports_method("textDocument/formatting") then
+-- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+-- 		vim.api.nvim_create_autocmd("BufWritePre", {
+-- 			group = augroup,
+-- 			buffer = bufnr,
+-- 			callback = function()
+-- 				lsp_formatting(bufnr)
+-- 			end,
+-- 		})
+-- 	end
+-- end
+
+null_ls.setup({
+	debug = false,
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.code_actions.shellcheck,
+	},
+	-- 保存自动格式化
+	on_attach = function(client)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					lsp_formatting(bufnr)
+				end,
+			})
+		end
+	end,
+})
